c970e77f19898ea54a817b16a9bf8198ee822ab9
abazunts home <bazunc@gmail.com>
2019-09-14 23:18:28 +0500

    Updates

---
+++ precache-manifest.ae3ded31c1f4a827a805ba088c37cb29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700161a46300a2b45b065628efb296cb",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "e93b584ea758138c49e5",
+    "url": "/tasks/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "55f9272fcae8a9c9921c",
+    "url": "/tasks/static/js/2.add498f7.chunk.js"
+  },
+  {
+    "revision": "e93b584ea758138c49e5",
+    "url": "/tasks/static/js/main.9beaf787.chunk.js"
+  },
+  {
+    "revision": "608fd2d05fa64894b2b9",
+    "url": "/tasks/static/js/runtime~main.a1540b94.js"
+  }
+]);
